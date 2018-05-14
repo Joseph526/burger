087bd5dcbe834360c24f971791c58aa95acc98ea
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-13 22:55:30 -0400

    Building ORM

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,16 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// ORM object to hold SQL statement methods
+var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+};
 
 // Export the orm object for the model
 module.exports = orm;
