7c9484808f0d25d5b5b20e9bd389f8a94fc9d693
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-13 23:45:46 -0400

    Building controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model
+var burger = require("../models/burger.js");
+
+// Export the routes
+module.exports = router;
