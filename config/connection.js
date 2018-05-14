9db2ab2dda3dd746ecf6ff51f0f281bae2a860a5
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-13 22:42:56 -0400

    Correction to MySQL connection

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  port: 3306,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PW,
-  database: process.env.MYSQL_DB
+    port: 3306,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PW,
+    database: process.env.MYSQL_DB
 });
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Require dependencies
+require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
