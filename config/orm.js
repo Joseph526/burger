7cf24b2eb7b9a3ca91fd032e8af705cac3ac029a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-13 23:25:18 -0400

    Completed ORM

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,25 @@ function printQuestionMarks(num) {
     return arr.toString();
 }
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+    
+    // loop through the keys and push the key/value as a string into arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+        arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 // ORM object to hold SQL statement methods
 var orm = {
     selectAll: function(tableInput, cb) {
@@ -29,6 +48,18 @@ var orm = {
         queryString += ");";
 
         connection.query(queryString, vals, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+        queryString += "SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             cb(result);
         });
