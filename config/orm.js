f07fae0983f9ae78f0674e993d4c75f9ebad7f73
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-13 23:10:07 -0400

    Added insert statement to ORM

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,34 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
 // ORM object to hold SQL statement methods
 var orm = {
     selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
         connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += " )";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ");";
+
+        connection.query(queryString, vals, function(err, result) {
             if (err) throw err;
             cb(result);
         });
