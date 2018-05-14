af0d6ad05828a38f06b091b214ba492bf5f9cf17
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-14 00:11:21 -0400

    Added get all route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,15 @@ var router = express.Router();
 // Import the model
 var burger = require("../models/burger.js");
 
+// Declare the routes
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
 // Export the routes
 module.exports = router;
