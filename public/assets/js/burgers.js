8bf3e1ee3bd7a94ba2ace2ad87dc79b5db8a475f
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-14 23:11:07 -0400

    Fixed boolean error

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    //
+    $("#btnSubmit").on("click", function(event) {
+        event.preventDefault();
+
+        // Capture user input
+        var newBurger = {
+            burger_name: $("#addBurger").val().trim(),
+            devoured: 0
+        };
+
+        // Send the POST request
+        $.post("/api/burgers", newBurger)
+            .then(function() {
+                location.reload();
+            });
+    });
 });
