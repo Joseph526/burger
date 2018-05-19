4149c59f2b3e4fff7a6db1e7d62a8eca157f63b7
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-19 18:17:20 -0400

    Configured JawsDB for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PW,
-    database: process.env.MYSQL_DB
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PW,
+        database: process.env.MYSQL_DB
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
